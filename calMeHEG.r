2b8ecd804003086fb536556eabf37c8b5a25a941
Huichuan Yu <91016308+hyu020@users.noreply.github.com>
2024-12-04 15:52:01 +0800

    Add files via upload

---
+++ calMeHEG.r
@@ -0,0 +1,18 @@
+##calculate different versions of MeHEG
+calMeHEG <- function(x) {
+data <- x
+library(matrixStats)
+#coef6s7
+mydata<-data.frame(data[row.names(coef6s7),])
+mydeconv<-data.frame(matrix(nrow=ncol(mydata),ncol=0))
+for (i in 1:ncol(coef6s7)) {
+mydata2 <- mydata*coef6s7[,i]
+mydeconv[,colnames(coef6s7)[i]] <- colSums(mydata2, na.rm = TRUE)
+}
+row.names(mydeconv) <- colnames(mydata)
+mydeconv$MeHEG.g6s7 <- rowSds(as.matrix(mydeconv),na.rm = TRUE)
+mydeconv$MeHEG.g6s7m <- mydeconv$MeHEG.g6s7/rowMeans(as.matrix(mydeconv[,-ncol(mydeconv)]),na.rm = TRUE)
+mydeconvolution <- mydeconv[,"MeHEG.g6s7m"]
+return(mydeconvolution)
+}#over
+
